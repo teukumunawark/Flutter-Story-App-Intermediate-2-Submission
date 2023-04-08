9825e8c56a3c83e801dfe09d0336582c52f1bec3
teukumunawark <munawartmk@gmail.com>
2023-04-08 20:28:47 +0700

    adding stories repo in domain

--- lib/domain/repositories/story_repository.dart
+++ lib/domain/repositories/story_repository.dart
@@ -1,11 +1,15 @@
 import 'package:dartz/dartz.dart';
 
-import '../../application/models/story_create_model.dart';
+import '../../application/models/story/story_model.dart';
 import '../entities/story_enitities.dart';
 import '../exceptions.dart';
 
 abstract class StoryRepository {
-  Future<Either<Failure, List<StoryResponseEntity>>> getStories(String token);
+  Future<Either<Failure, List<StoryResponseEntity>>> getStories([
+    String? token,
+    int page = 1,
+    int size = 10,
+  ]);
   Future<Either<Failure, void>> createStory(
     StoryCreateModel story,
     String toke,
