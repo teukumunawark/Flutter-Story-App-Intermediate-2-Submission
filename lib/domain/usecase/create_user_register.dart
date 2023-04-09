d5801a7c552bab5ef611bde7bf954a73f8b7252e
teukumunawark <munawartmk@gmail.com>
2023-04-09 23:25:44 +0700

    adding user register usecase

--- lib/domain/usecase/create_user_register.dart
+++ lib/domain/usecase/create_user_register.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 
-import '../../application/models/user_create_model.dart';
+import '../../application/models/auth/auth_model.dart';
 import '../exceptions.dart';
 import '../repositories/user_repository.dart';
 
